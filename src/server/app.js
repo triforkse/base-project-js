9e1b4155d9141eb99c1e487d3bf21f3979c587b4
Max Malm <benjick@dumfan.net>
2015-11-06 09:09:07 +0100

    Made auth into a module

--- src/server/app.js
+++ src/server/app.js
@@ -1,19 +1,9 @@
 import express from 'express';
-import passport from 'passport';
-import {BasicStrategy} from 'passport-http';
 import logger from './logger';
 import path from 'path';
+import authMiddleware from './auth';
 
 const app = express();
-
-passport.use(new BasicStrategy((username, password, done) => {
-  if (username === 'admin' && password === 'password') {
-    return done(null, {username, password});
-  }
-  return done(null, false, { message: 'Who are you again?' });
-}));
-
-const authMiddleware = passport.authenticate('basic', { session: false });
 
 app.use('/static', express.static(path.join(__dirname, 'public')));
 
