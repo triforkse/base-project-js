ce2c27d39447c4bb39e77bb5e2b21b40128d8d4b
Max Malm <benjick@dumfan.net>
2015-12-15 13:43:21 +0100

    Test API connectivity

--- e2e/scripts/run.sh
+++ e2e/scripts/run.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-printenv
-
 if [ "$CLIENT_ADDR" = "" ]
 then
    export CLIENT_ADDR=$APP_CLIENT_1_PORT_3001_TCP_ADDR
 fi
 
+if [ "$API_ADDR" = "" ]
+then
+   export API_ADDR=$APP_API_1_PORT_3000_TCP_ADDR
+fi
+
 echo "Accessing Web UI on $CLIENT_ADDR"
+echo "Accessing API on $API_ADDR"
 
 Xvfb -ac -screen scrn 1280x2000x24 :9.0 &
 export DISPLAY=:9.0
---
+++ e2e/test/api.spec.js
@@ -0,0 +1,32 @@
+/* globals describe beforeEach afterEach it expect */
+import Nightmare from 'nightmare';
+require('mocha-generators').install();
+
+// Get rid of a warning.
+process.setMaxListeners(0);
+
+// Utils
+const innerHTML = (selector) => document.querySelector(selector).innerHTML;
+const innerText = (selector) => innerHTML(selector).trim();
+
+describe('API endpoint', function () {
+  this.timeout(15000);
+
+  const hostname = process.env.API_ADDR || 'localhost';
+  const base = 'http://' + hostname + ':3000';
+
+  let nightmare;
+
+  beforeEach(() => {
+    nightmare = Nightmare();
+  });
+
+  afterEach(function * () {
+    yield nightmare.end();
+  });
+
+  it("is reachable and returns OK", function * () {
+    const title = yield nightmare.goto(base + '/api/1.0/status').evaluate(() => document.body.innerText);
+    expect(title).to.equal('OK');
+  });
+});
